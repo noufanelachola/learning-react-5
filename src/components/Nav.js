c2d0f92984bf30c7e465168c0fcde9a3de528492
noufanelachola <noufansadiqelachola@gmail.com>
2023-08-08 09:24:28 +0530

    second

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+    return(
+        <div className="navCont">
+            <div className="navLogo">
+                <img src="/images/logo.png" alt="Logo" />
+                <p>Meme Generator</p>
+            </div>
+            <p>React course - project 5 </p>
+        </div>
+    );
+}
+
+export default Nav;
