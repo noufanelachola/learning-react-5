dbe978abe7197742fd0f13549e316274970d2c6f
noufanelachola <noufansadiqelachola@gmail.com>
2023-08-09 08:33:31 +0530

    upto url fn

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,23 @@
 import React from "react";
+import Meme from "./Meme";
 
 const Input = () => {
+
+    let url;
+    function getMemeImage() {
+        let memesArray = Meme.data.memes;
+        let randomNumber = Math.floor(Math.random() * memesArray.length);
+        url = memesArray[randomNumber].url;
+        console.log(url);
+    } 
+    
     return (
         <div className="inputCont">
             <div className="inputBox">
                 <input type="text" placeholder="first" className="inputBoxOne"/>
                 <input type="text" placeholder="second" className="inputBoxSecond"/>
             </div>
-            <button className="inputBtn">Get a new meme image</button>
+            <button className="inputBtn" onClick={getMemeImage}>Get a new meme image</button>
         </div>
     );
 }
