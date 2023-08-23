7a5fa8fe0495f85556bd347c5dc6f73277063450
noufanelachola <noufansadiqelachola@gmail.com>
2023-08-23 07:14:44 +0530

    added text to meme img

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,23 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 import Meme from "./Meme";
 
 const Input = () => {
+    const [meme , setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        memeImage: ""
+    });
 
     let url;
     function getMemeImage() {
         let memesArray = Meme.data.memes;
         let randomNumber = Math.floor(Math.random() * memesArray.length);
         url = memesArray[randomNumber].url;
-        console.log(url);
+        setMeme(prevMeme => {
+            return (
+                {
+                    ...prevMeme,
+                    memeImage: url
+                }
+            )
+        });
     } 
+
+    function handleChange(event) {
+        const {name,value} = event.target;
+        setMeme(prevMeme => {
+            return ({
+                ...prevMeme,
+                [name] : value
+            })
+        })
+    }
     
     return (
         <div className="inputCont">
             <div className="inputBox">
-                <input type="text" placeholder="first" className="inputBoxOne"/>
-                <input type="text" placeholder="second" className="inputBoxSecond"/>
+                <input type="text" placeholder="first" className="inputBoxOne" onChange={handleChange} name="topText" value={meme.topText}/>
+                <input type="text" placeholder="second" className="inputBoxSecond" onChange={handleChange} name="bottomText" value={meme.bottomText}/>
             </div>
+           
             <button className="inputBtn" onClick={getMemeImage}>Get a new meme image</button>
+            
+            <div className="meme">
+                <img src={meme.memeImage} className="memeImage" alt="meme"/>
+                <h2 className="memeText memeText-top">{meme.topText}</h2>
+                <h2  className="memeText memeText-bottom">{meme.bottomText}</h2>
+            </div>
+
         </div>
     );
 }
